0d57a453a46c62a0b8c8bb527fd343c77fd54691
yujiseok <koesijuy@gmail.com>
2023-02-05 14:17:36 +0900

    refactor: todo provider update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import Globalstyles from "./components/Globalstyles";
 import AuthProvider from "./context/AuthProvider";
-import TodoProvider from "./context/TodoProvider";
 import Router from "./Router";
 
 const App = () => {
   return (
     <AuthProvider>
-      <TodoProvider>
-        <Router />
-        <Globalstyles />
-      </TodoProvider>
+      <Router />
+      <Globalstyles />
     </AuthProvider>
   );
 };
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/common/Layout";
+import TodoProvider from "./context/TodoProvider";
 import Root from "./pages/Root";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
@@ -13,7 +14,14 @@ const Router = () => {
           <Route path="/" element={<Root />} />
           <Route path="signin" element={<SignIn />} />
           <Route path="signup" element={<SignUp />} />
-          <Route path="todo" element={<Todo />} />
+          <Route
+            path="todo"
+            element={
+              <TodoProvider>
+                <Todo />
+              </TodoProvider>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
